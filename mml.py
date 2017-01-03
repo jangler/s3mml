84b3923695c6e8a068195b6fe468ce552b9223b4
Brandon Mulcahy <brandon@jangler.info>
2017-01-03 12:53:14 -0500

    Implement Cxx and ties

--- mml.py
+++ mml.py
@@ -23,7 +23,7 @@ def notestr(cell, prevcell, f):
         print(NOTE_NAMES[cell[0] % 16], end='', file=f)
 
 
-def lenstr(notelen, cell):
+def lenstr(notelen):
     # one row is a 16th note
     lengths = []
     while notelen >= 16:
@@ -41,11 +41,7 @@ def lenstr(notelen, cell):
     while notelen >= 1:
         notelen -= 1
         lengths.append('16')
-    if cell == None or cell[0] & 128:
-        joiner = '&r'
-    else:
-        joiner = '&%s' % NOTE_NAMES[cell[0] % 16]
-    return joiner.join(lengths)
+    return '&&'.join(lengths)
 
 
 with open(sys.argv[1], 'rb') as f:
@@ -54,31 +50,36 @@ with open(sys.argv[1], 'rb') as f:
 print('#Title    %s' % module.title)
 print('#Filename .M2')
 
-print('\nABCDEFGHI t%d\n' % (module.initialtempo * 6 // module.initialspeed))
+print('\nABCDEFGHI t%d\n' % (module.initialtempo * 3 // module.initialspeed))
 
 def print_pattern(pattern):
+    startrow, endrow = 0, -1  # Cxx stuff
+    cxx = False
     for channel in range(9):
         print(chr(65 + channel), end=' ')
         prevcell, notelen = None, 0
-        for row in pattern:
+        if endrow == -1:
+            endrow = len(pattern)
+        for i, row in enumerate(pattern[startrow:endrow]):
             cell = row[channel]
             if cell and cell[0]:
                 if notelen != 0:
                     if prevcell == None:
                         print('r', end='')
-                    print(lenstr(notelen, prevcell), end=' ')
+                    print(lenstr(notelen), end=' ')
                 notestr(cell, prevcell, sys.stdout)
                 prevcell, notelen = cell, 0
-            elif notelen == 32:
-                if prevcell == None:
-                    print('r', end='')
-                print(lenstr(notelen, prevcell), end=' ')
-                prevcell, notelen = None, 0
             notelen += 1
+            if cell and cell[3]:
+                if cell[3] == 3:  # Cxx
+                    startrow, endrow, cxx = cell[4], i+1, True
+                    break
+        if not cxx:
+            startrow, endrow = 0, -1
         if notelen != 0:
             if prevcell == None:
                 print('r', end='')
-            print(lenstr(notelen, prevcell), end=' ')
+            print(lenstr(notelen), end=' ')
         print() # newline
     print()  # newline
 
